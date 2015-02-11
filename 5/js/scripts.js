019327814fa469bb24f0551d13f739b0c6d8a2d0
LLstarla <lgentry1@engr-134-23.eastern.ewu.edu>
2015-02-11 14:59:46 -0800

    last

--- 5/js/scripts.js
+++ 5/js/scripts.js
@@ -1,7 +1,15 @@
 // Check for User's Birthday
 // REF: http://codepen.io/manikoth/pen/BymXwR?editors=101
 // REF: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+
+//Get date
+
 var date = new Date();
+
+console.log(date);
+
+//Store Date
 
 var birthday = {
   month: date.getMonth(),
@@ -10,10 +18,12 @@ var birthday = {
   user: ['4/6', '9/23', '10/1', '1/11', '12/24', '7/7']
 };
 
-// Get & Store Today's Date
-// Remember 0 is January, 1 is February, etc.
-var today;
 
+// Remember 0 is January, 1 is February, etc.
 // *IF* users stored birthday is equal to: "1/11" 
 // Add class `highlight`
 // REF: http://api.jquery.com/addclass
+
+if (birthday.check == '1/11') {
+  $('.user3').addClass('.highlight';
+};
