e28866e30e45dbb2ca6ad528838bf8bd0f2a7f00
LLstarla <lgentry1@engr-134-23.eastern.ewu.edu>
2015-02-11 13:42:51 -0800

    2. css

--- 2/js/scripts.js
+++ 2/js/scripts.js
@@ -1,13 +1,19 @@
 // Close Button
 
+$('.message .close').on('click', function() {
+  $(this).closest('.message').fadeOut();
+});
 
 // Previous Number of Messages
 var prevNumMsg = 35;
+
 // New Number of Messages
 var newNumMsg = 123;
+
 // Create variable, `totalMsg` to store Total Messages
 // Add (sum) of Previous and New Messages
 var totalMsg;
+
 
 // Output to red Messages badge
 // REF: http://api.jquery.com/text/
