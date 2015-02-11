f85e668bdcd8ca32cdb511f218c4c68a20d03748
LLstarla <lgentry1@engr-134-23.eastern.ewu.edu>
2015-02-11 13:52:10 -0800

    2. done

--- 2/js/scripts.js
+++ 2/js/scripts.js
@@ -12,8 +12,10 @@ var newNumMsg = 123;
 
 // Create variable, `totalMsg` to store Total Messages
 // Add (sum) of Previous and New Messages
-var totalMsg;
+var totalMsg = prevNumMsg + newNumMsg;
 
 
 // Output to red Messages badge
 // REF: http://api.jquery.com/text/
+
+$('.msg').text(totalMsg);
