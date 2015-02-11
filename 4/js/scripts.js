926fcb57ba97357509e7656c834057f86f46c853
LLstarla <lgentry1@engr-134-23.eastern.ewu.edu>
2015-02-11 14:38:42 -0800

    4. done

--- 4/js/scripts.js
+++ 4/js/scripts.js
@@ -1,21 +1,44 @@
 // Change Day and Night reading modes
 
-
 // Upon Clicking "Nightime"
 // REF: http://api.jquery.com/click/
-
+$( "#targetnight" ).click(function() {
+  
 // remove `day` class
 // REF: http://api.jquery.com/removeclass/
+$('body').removeClass('day');
 
 // Add `night` class
 // REF: http://api.jquery.com/addclass/
+$('body').addClass('night');
+
+});
+
 
 
 // Upon Clicking "Daytime"
 // REF: http://api.jquery.com/click/
-
+$( "#targetday" ).click(function() {
+  
 // remove `night` class
 // REF: http://api.jquery.com/removeclass/
+$('body').removeClass('night');
 
 // Add `day` class
 // REF: http://api.jquery.com/addclass/
+
+$('body').addClass('day');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
